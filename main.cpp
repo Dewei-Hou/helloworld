c11a478f00123addcb8f877ee31aa38131f63d67
Dewei-Hou <a1075543@mail.nuk.edu.tw>
2021-03-28 23:31:07 +0800

    1234

--- main.cpp
+++ main.cpp
@@ -3,9 +3,7 @@ using namespace std;
 
 int main(){
     printf("Hello World");
-    printf("111");
-    printf("222");
-    int a=4;
+    printf("12345");
     int x=1,y=2;
     int z=x+y;
     system("PAUSE");
